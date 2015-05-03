2fb8c91087c3e151a6c9afcf3097a0b766b35408
Kulyash <kulyash169@gmail.com>
2015-05-04 00:01:03 +0600

    change BE

--- disqus_be/core_app/api.py
+++ disqus_be/core_app/api.py
@@ -1,6 +1,20 @@
 """This module contains API resource classes"""
 from tastypie.resources import ModelResource
+# from django.contrib.auth.models import User
 from core_app.models import Comment
 from tastypie.authorization import Authorization
-from tastypie import fields
+# from tastypie import fields
 from core_app import utils
+from tastypie.resources import ModelResource, ALL, ALL_WITH_RELATIONS
+
+
+#create a commentResource class
+class CommentResource(ModelResource):
+    class Meta:
+    	limit=2000
+        queryset = Comment.objects.all()
+        resource_name = 'comment'
+        authorization = Authorization()
+        filtering = {
+            'pub_date': ['exact', 'lt', 'lte', 'gte', 'gt'],
+        }
--- disqus_be/core_app/models.py
+++ disqus_be/core_app/models.py
@@ -3,3 +3,12 @@ from django.db import models
 from django.utils import timezone
 from core_app import utils
 from hashlib import md5
+
+#create a comment class
+class Comment(models.Model):
+    text = models.CharField(max_length = 1000)
+    pub_time = models.DateTimeField(auto_now_add = timezone.now)
+    is_public = models.BooleanField(default = True)
+    author_title = models.CharField(max_length = 1000)
+    image = models.CharField(max_length = 1000)
+    
